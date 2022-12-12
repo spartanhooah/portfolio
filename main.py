2dd9f535b8c583f5825e370f91f90c6e5f7fe474
Ben Frey <spartanhooah@gmail.com>
2022-12-11 21:00:52 -0500

    add a header for the app columns

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ with column2:
     st.title("Ben Frey")
     content = """
     I am a Java developer with experience in Spring Boot APIs and Spock testing.
+    I graduated in 2016 with a Master of Science in Computer Science 
+    from Michigan State University.
     """
     st.info(content)
 
+st.write("Below you can find some of the apps I have built in Python. Feel free to contact me!")
