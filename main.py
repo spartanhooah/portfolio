9c0a0cf45adee18d7c5c83b842ff2793bc83c5e5
Ben Frey <spartanhooah@gmail.com>
2022-12-12 19:52:19 -0500

    fill columns with content

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import streamlit as st
+import pandas
 
 st.set_page_config(layout="wide")
+
+
+def create_row(row):
+    st.header(row["title"])
+    st.write(row["description"])
+    st.image(f"images/{row['image']}")
+    st.write(f"[Source Code]({row['url']})")
+
 
 column1, column2 = st.columns(2)
 
 with column1:
-    st.image("images/photo.jpg")
+    st.image("images/photo.jpg", width=600)
 
 with column2:
     st.title("Ben Frey")
@@ -17,3 +26,18 @@ with column2:
     st.info(content)
 
 st.write("Below you can find some of the apps I have built in Python. Feel free to contact me!")
+
+column3, empty, column4 = st.columns([1.5, 0.5, 1.5])
+
+data = pandas.read_csv("data.csv", sep=";")
+
+
+with column3:
+    for index, row in data.iterrows():
+        if index % 2:
+            create_row(row)
+
+with column4:
+    for index, row in data.iterrows():
+        if not index % 2:
+            create_row(row)
